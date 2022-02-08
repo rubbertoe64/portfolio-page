38228fcfbd4a8686a86e3b09110a1383cd3e7938
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-02-07 18:31:37 -0800

    added js to make images be clickable and send to website, made slight chagnes to css style

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+let githubIcon1 = document.getElementById('githubIcon1')
+let worldIcon1 = document.getElementById('worldIcon1')
+let githubIcon2 = document.getElementById('githubIcon2')
+let worldIcon2 = document.getElementById('worldIcon2')
+
+githubIcon1.addEventListener("click", (e) => {
+    window.open("https://github.com/rubbertoe64/homework-1", "_blank") ;
+})
+
+worldIcon1.addEventListener("click", (e) => {
+    window.open("https://rubbertoe64.github.io/homework-1/", "_blank") ;
+})
+
+githubIcon2.addEventListener("click", (e) => {
+    window.open("https://github.com/Will-Pikus/Restroom-Finder", "_blank") ;
+})
+
+worldIcon2.addEventListener("click", (e) => {
+    window.open("https://will-pikus.github.io/Restroom-Finder/", "_blank") ;
+})
+
+
+
+
+
+
