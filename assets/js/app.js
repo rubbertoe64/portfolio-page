f5bbaf4624e6db8e846ec3502a9f1fd6e82b6507
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-02-07 19:14:26 -0800

    added calendar page with styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,12 @@ let githubIcon1 = document.getElementById('githubIcon1')
 let worldIcon1 = document.getElementById('worldIcon1')
 let githubIcon2 = document.getElementById('githubIcon2')
 let worldIcon2 = document.getElementById('worldIcon2')
+let githubIcon3 = document.getElementById('githubIcon3')
+let worldIcon3 = document.getElementById('worldIcon3')
+let githubIcon4 = document.getElementById('githubIcon4')
+let worldIcon4 = document.getElementById('worldIcon4')
+
+
 
 githubIcon1.addEventListener("click", (e) => {
     window.open("https://github.com/rubbertoe64/homework-1", "_blank") ;
@@ -11,6 +17,8 @@ worldIcon1.addEventListener("click", (e) => {
     window.open("https://rubbertoe64.github.io/homework-1/", "_blank") ;
 })
 
+
+
 githubIcon2.addEventListener("click", (e) => {
     window.open("https://github.com/Will-Pikus/Restroom-Finder", "_blank") ;
 })
@@ -18,6 +26,28 @@ githubIcon2.addEventListener("click", (e) => {
 worldIcon2.addEventListener("click", (e) => {
     window.open("https://will-pikus.github.io/Restroom-Finder/", "_blank") ;
 })
+
+
+
+githubIcon3.addEventListener("click", (e) => {
+    window.open("https://github.com/rubbertoe64/weather-page", "_blank") ;
+})
+
+worldIcon3.addEventListener("click", (e) => {
+    window.open("https://rubbertoe64.github.io/weather-page/", "_blank") ;
+})
+
+
+
+githubIcon4.addEventListener("click", (e) => {
+    window.open("https://github.com/rubbertoe64/work-calendar", "_blank") ;
+})
+
+worldIcon4.addEventListener("click", (e) => {
+    window.open("https://rubbertoe64.github.io/weather-page/", "_blank") ;
+})
+
+
 
 
 
