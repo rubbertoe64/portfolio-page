3ebe41864c33716361b24d54dbd329816b48a020
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-04-04 16:04:27 -0700

    created new event listeners for added projects

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,19 +6,23 @@ let githubIcon3 = document.getElementById('githubIcon3')
 let worldIcon3 = document.getElementById('worldIcon3')
 let githubIcon4 = document.getElementById('githubIcon4')
 let worldIcon4 = document.getElementById('worldIcon4')
+let githubIcon5 = document.getElementById('githubIcon5')
+let worldIcon5 = document.getElementById('worldIcon5')
+let githubIcon6 = document.getElementById('githubIcon6')
+let worldIcon6 = document.getElementById('worldIcon6')
 
 
-
+// 1
 githubIcon1.addEventListener("click", (e) => {
-    window.open("https://github.com/rubbertoe64/homework-1", "_blank") ;
+    window.open("https://github.com/rubbertoe64/note-taker", "_blank") ;
 })
 
 worldIcon1.addEventListener("click", (e) => {
-    window.open("https://rubbertoe64.github.io/homework-1/", "_blank") ;
+    window.open("https://note-taker-api-online.herokuapp.com/", "_blank") ;
 })
 
 
-
+// 2
 githubIcon2.addEventListener("click", (e) => {
     window.open("https://github.com/Will-Pikus/Restroom-Finder", "_blank") ;
 })
@@ -28,7 +32,7 @@ worldIcon2.addEventListener("click", (e) => {
 })
 
 
-
+// 3
 githubIcon3.addEventListener("click", (e) => {
     window.open("https://github.com/rubbertoe64/weather-page", "_blank") ;
 })
@@ -38,14 +42,34 @@ worldIcon3.addEventListener("click", (e) => {
 })
 
 
-
+// 4
 githubIcon4.addEventListener("click", (e) => {
     window.open("https://github.com/rubbertoe64/work-calendar", "_blank") ;
 })
 
 worldIcon4.addEventListener("click", (e) => {
-    window.open("https://rubbertoe64.github.io/weather-page/", "_blank") ;
+    window.open("https://rubbertoe64.github.io/work-calendar/", "_blank") ;
 })
+
+
+// 5
+githubIcon5.addEventListener("click", (e) => {
+    window.open("https://github.com/rubbertoe64/employee-tracker", "_blank") ;
+})
+
+// worldIcon4.addEventListener("click", (e) => {
+//     window.open("https://rubbertoe64.github.io/weather-page/", "_blank") ;
+// })
+
+
+// 6
+githubIcon6.addEventListener("click", (e) => {
+    window.open("https://github.com/rubbertoe64/e-commerce-backend", "_blank") ;
+})
+
+// worldIcon6.addEventListener("click", (e) => {
+//     window.open("https://rubbertoe64.github.io/weather-page/", "_blank") ;
+// })
 
 
 
